f4456053a5f942aa2d8ef528745bd8788feebebb
xingyuan_chen <2677108705@qq.com>
2023-04-30 22:42:22 +0800

    Add feature: print! and println!

--- lab1os/src/entry.asm
+++ lab1os/src/entry.asm
@@ -1,5 +1,13 @@
     .section .text.etry
     .globl _start
 _start:
-    li x1,100
+    la sp, book_stark_top
+    call rust_main
+
+    .section .bss.stack
+    .globl book_stark_lower_bound
+book_stark_lower_bound:
+    .space 4096 * 16
+    .globl book_stark_top
+book_stark_top:
     
--- lab1os/src/main.rs
+++ lab1os/src/main.rs
@@ -1,7 +1,29 @@
 #![no_std]                  //不使用标准库
 #![no_main]
 
-use core::arch::global_asm;                 //不从main开始，直接编译
+use core::arch::global_asm;
+
+use sbi::console_putchar;                 //不从main开始，直接编译
 mod lang_items;
+mod sbi;
+mod console;
 
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clean_bss();
+    println!("Hello, AmyYin!");
+    println!("Hello, MiseZi!");
+    loop {}
+}
+
+fn clean_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
