b0eeef52a571a1731b95ea71d8a044e64cefc435
MiseZi <2677108705@qq.com>
2023-06-13 17:57:11 +0800

    add block cache

--- easy-fs/src/block_dev.rs
+++ easy-fs/src/block_dev.rs
@@ -1,4 +1,6 @@
+use core::any::Any;
+
 pub trait BlockDevice: Send + Sync + Any {
-    fn read_block(&Self, block_id: usize, buf: &mut [u8]);
-    fn write_clock(&Self, block_id: usize, buf: &[u8]);
+    fn read_block(&self, block_id: usize, buf: &mut [u8]);
+    fn write_clock(&self, block_id: usize, buf: &[u8]);
 }
--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod block_cache;
+mod block_dev;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+extern crate alloc;
+
+
+pub const BLOCK_SZ: usize = 512;
