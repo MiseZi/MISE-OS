b5e24c9993f14dad94efc58b30ddf32bac4ad7c3
MiseZi <2677108705@qq.com>
2023-06-13 15:13:24 +0800

    build lab6os basement

---
+++ lab6os/src/task/mod.rs
@@ -0,0 +1,108 @@
+//! Task management implementation
+//!
+//! Everything about task management, like starting and switching tasks is
+//! implemented here.
+//!
+//! A single global instance of [`TaskManager`] called `TASK_MANAGER` controls
+//! all the tasks in the operating system.
+//!
+//! Be careful when you see `__switch` ASM function in `switch.S`. Control flow around this function
+//! might not be what you expect.
+
+mod context;
+mod switch;
+#[allow(clippy::module_inception)]
+mod task;
+mod pid;
+pub mod manager;
+pub mod processor;
+
+use crate::loader::get_app_data_by_name;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+use lazy_static::*;
+use task::{TaskControlBlock, TaskStatus};
+
+
+pub use context::TaskContext;
+
+use self::manager::add_task;
+use self::processor::{take_current_task, schedule};
+
+/// The task manager, where all the tasks are managed.
+///
+/// Functions implemented on `TaskManager` deals with all task state transitions
+/// and task context switching. For convenience, you can find wrappers around it
+/// in the module level.
+///
+/// Most of `TaskManager` are hidden behind the field `inner`, to defer
+/// borrowing checks to runtime. You can see examples on how to use `inner` in
+/// existing functions on `TaskManager`.
+
+/// The task manager inner in 'UPSafeCell'
+struct TaskManagerInner {
+    /// task list
+    tasks: Vec<TaskControlBlock>,
+    /// id of current `Running` task
+    current_task: usize,
+}
+
+lazy_static! {
+    pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
+        TaskControlBlock::new(get_app_data_by_name("initproc").unwrap())
+    );
+}
+
+
+/// Suspend the current 'Running' task and run the next task in task list.
+pub fn suspend_current_and_run_next() {
+    let task = take_current_task().unwrap();
+
+    let mut task_inner = task.inner_exclusive_access();
+    
+    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+
+    add_task(task);
+    schedule(task_cx_ptr);
+}
+
+/// Exit the current 'Running' task and run the next task in task list.
+pub fn exit_current_and_run_next(exit_code: i32) {
+    // take from Processor
+    let task = take_current_task().unwrap();
+    // **** access current TCB exclusively
+    let mut inner = task.inner_exclusive_access();
+    // Change status to Zombie
+    inner.task_status = TaskStatus::Zombie;
+    // Record exit code
+    inner.exit_code = exit_code;
+    // do not move to its parent but under initproc
+
+    // ++++++ access initproc TCB exclusively
+    {
+        let mut initproc_inner = INITPROC.inner_exclusive_access();
+        for child in inner.children.iter() {
+            child.inner_exclusive_access().parent = Some(Arc::downgrade(&INITPROC));
+            initproc_inner.children.push(child.clone());
+        }
+    }
+    // ++++++ stop exclusively accessing parent PCB
+
+    inner.children.clear();
+    // deallocate user space
+    inner.memory_set.recycle_data_pages();
+    drop(inner);
+    // **** stop exclusively accessing current PCB
+    // drop task manually to maintain rc correctly
+    drop(task);
+    // we do not have to save task context
+    let mut _unused = TaskContext::zero_init();
+    schedule(&mut _unused as *mut _);
+}
+
+pub fn add_initproc() {
+    add_task(INITPROC.clone());
+}
